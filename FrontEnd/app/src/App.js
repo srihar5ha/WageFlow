1d02f5e9f30a4619f71d04b83d7ad7fb58668b66
srihar5ha <sri3har5ha@gmail.com>
2023-10-15 21:43:48 +0530

    Auth kit mumbai configured

--- FrontEnd/app/src/App.js
+++ FrontEnd/app/src/App.js
@@ -1,118 +1,92 @@
-// @ts-nocheck
 
 
 import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import abi from "./contracts/PayV1.json";
 
-import { Web3AuthModalPack, Web3AuthConfig } from '@safe-global/auth-kit'
-import { Web3AuthOptions } from '@web3auth/modal'
-import { OpenloginAdapter } from '@web3auth/openlogin-adapter';
-import { CHAIN_NAMESPACES } from "@web3auth/base";
-import { WALLET_ADAPTERS } from '@web3auth/base';
 
-// import { initWeb3Auth, signInWithWeb3Auth, signOutWithWeb3Auth, getWeb3AuthProvider } from './components/Authkit';
-// import { initWeb3Auth,signInWithWeb3Auth } from './components/Authkit';
-
-// import web3AuthFunctions from ".components/Authkit.ts";
-
-
-const contractAddress="0x565eB9B0fE93D307eD5aC33DcD5cb81896498d18"; //later use .env
-
-const {ethers} = require("ethers");
-
-
-  const options = {
-    clientId: process.env.REACT_APP_WEB3AUTH_CLIENT_ID,
-    web3AuthNetwork: 'testnet',
-    chainConfig: {
-      chainNamespace:CHAIN_NAMESPACES.EIP155,
-      chainId: 0x1,
-      rpcTarget: "https://rpc.ankr.com/eth"
-    },
-    uiConfig: {
-      theme: 'dark',
-      loginMethodsOrder: ['google', 'facebook']
-    }
-  }
-  
-  const modalConfig = {
-    [WALLET_ADAPTERS.TORUS_EVM]: {
-      label: 'torus',
-      showOnModal: false
-    },
-    [WALLET_ADAPTERS.METAMASK]: {
-      label: 'metamask',
-      showOnDesktop: true,
-      showOnMobile: false
-    }
-  }
-  
-  const openloginAdapter = new OpenloginAdapter({
-    loginSettings: {
-      mfaLevel: 'mandatory'
-    },
-    adapterSettings: {
-      uxMode: 'popup',
-      whiteLabel: {
-        name: 'Safe'
-      }
-    }
-  })
-  
-  const web3AuthModalPack = new Web3AuthModalPack({
-    txServiceUrl: 'https://safe-transaction-polygon.safe.global',
-  })
-  
-  // await web3AuthModalPack.init({options,adapters: [openloginAdapter], modalConfig })
-  async function signInWithWeb3Auth() {
-    await web3AuthModalPack.signIn();
-  }
-
-
-// Logout
-async function signOutWithWeb3Auth() {
-    await web3AuthModalPack.signOut();
-  }
-
-// Get the provider
-function getWeb3AuthProvider() {
-    return web3AuthModalPack.getProvider();
-  }
-
-
+ import { options, modalConfig,openloginAdapter,web3AuthModalPack} from "./components/Authkit.ts";
 
 function App() {
+
+  const [eoaValue,setEoa]=useState(null);
+  const [provider,setProvider]=useState(null);
+  const [address,setAddress]=useState(null);
+  const [userInfo,setUserinfo]=useState(null);
+  const [safes,setSafes]=useState('');
+
+
+
   useEffect(() => {
-    console.log("init called");
-  
+    async function initModal(){
+    
+    await web3AuthModalPack.init({options, adapters: [openloginAdapter], modalConfig })
+    }
+    initModal();
   }, []);
+
+ 
 
   const handleSignIn = async () => {
     try {
-      // Call the signInWithWeb3Auth function from comp1.tsx
-      await web3AuthModalPack.init({options,adapters: [openloginAdapter], modalConfig })
+      
       const { eoa, safes } = await web3AuthModalPack.signIn()
-      console.log('Successfully signed in with Web3Auth,eoa,safes',eoa,safes);
+      console.log('Successfully signed in with Web3Auth, eoa,safes = ',eoa,safes);
+      
+      const newProvider = await web3AuthModalPack.getProvider();
+      const newAddress= await web3AuthModalPack.getAddress();
+      const newUserinfo= await web3AuthModalPack.getUserInfo();
+      console.log("new provider is ",newProvider);
+      console.log("new address ",newAddress);
+      console.log("new Usr info ",newUserinfo);
+      setEoa(eoa);
+      setProvider(newProvider);
+      setAddress(newAddress);
+      setUserinfo(newUserinfo);
+
     } catch (error) {
       console.error('Error signing in with Web3Auth', error);
     }
   } 
 
+  const handleSignOut= async() =>{
+    try{
+      await web3AuthModalPack.signOut();
+      setEoa(null);
+      setProvider(null);
+      setUserinfo(null);
+      setAddress(null);
+    }catch(error){
+      console.log("error signing out",error);
+    }
+  }
 
-
+ 
 
   return (
+    <>
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Click the button below to sign in with Web3Auth:</p>
 
         <button onClick={handleSignIn}>Sign In with Web3Auth</button>
+        {/* <p>
+          "Data "  {eoaValue ?  JSON.stringify(userInfo) : "no data"}
+        </p> */}
+        {eoaValue !== null && (
+          <div className='card'>
+            <p>eoa:  {eoaValue}</p>
+            <p>Address: {address}</p>
+          </div>
+        )}
+        <button onClick={handleSignOut}>SIgn Out</button>
         
       </header>
     </div>
+    </>
   );
 }
 
--- FrontEnd/app/src/Authkit.js
+++ FrontEnd/app/src/Authkit.js
@@ -2,17 +2,16 @@
 
 import { Web3AuthModalPack } from '@safe-global/auth-kit'
 import { OpenloginAdapter } from  '@web3auth/openlogin-adapter'
-import { CHAIN_NAMESPACES } from "@web3auth/base";
-import { WALLET_ADAPTERS } from '@web3auth/base';
+import { CHAIN_NAMESPACES, SafeEventEmitterProvider } from "@web3auth/base";
 
 
-const options: Web3AuthOptions = {
+const options= {
   clientId: process.env.REACT_APP_WEB3AUTH_CLIENT_ID,
-  web3AuthNetwork: "testnet",
+  web3AuthNetwork: 'testnet',
   chainConfig: {
     chainNamespace:CHAIN_NAMESPACES.EIP155,
-    chainId:'0x13881', //0x1,
-    rpcTarget: 'https://rpc-mumbai.maticvigil.com/',//"https://rpc.ankr.com/eth"
+    chainId: 0x1,
+    rpcTarget: "https://rpc.ankr.com/eth"
   },
   uiConfig: {
     theme: 'dark',
@@ -28,7 +27,7 @@ const modalConfig = {
   [WALLET_ADAPTERS.METAMASK]: {
     label: 'metamask',
     showOnDesktop: true,
-    showOnMobile: false,
+    showOnMobile: false
   }
 }
 
@@ -45,27 +44,37 @@ const openloginAdapter = new OpenloginAdapter({
 })
 
 const web3AuthModalPack = new Web3AuthModalPack({
- // txServiceUrl: "https://safe-transaction-polygon.safe.global"
+  txServiceUrl: 'https://safe-transaction-polygon.safe.global',
 })
 
-//await web3AuthModalPack.init({options,adapters: [openloginAdapter], modalConfig })
+// await web3AuthModalPack.init({
+//   options,
+//   adapters: [openloginAdapter],
+//   modalConfig
+// })
 
 
-// // Allow to login and get the derived EOA
-// async function signInWithWeb3Auth() {
-//     await web3AuthModalPack.signIn();
-//   }
+// Allow to login and get the derived EOA
+async function signInWithWeb3Auth() {
+    await web3AuthModalPack.signIn();
+  }
 
 
-// // Logout
-// async function signOutWithWeb3Auth() {
-//     await web3AuthModalPack.signOut();
-//   }
+// Logout
+async function signOutWithWeb3Auth() {
+    await web3AuthModalPack.signOut();
+  }
 
-// // Get the provider
-// function getWeb3AuthProvider() {
-//     return web3AuthModalPack.getProvider();
-//   }
+// Get the provider
+function getWeb3AuthProvider() {
+    return web3AuthModalPack.getProvider();
+  }
 
+  // const web3AuthFunctions = {
+  //   initWeb3Auth,
+  //   signInWithWeb3Auth,
+  //   signOutWithWeb3Auth,
+  //   getWeb3AuthProvider,
+  // };
 
   export {options,modalConfig,openloginAdapter,web3AuthModalPack};
