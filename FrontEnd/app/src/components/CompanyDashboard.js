05b2599615eef208a065e1f245d19239c1b9757c
srihar5ha <sri3har5ha@gmail.com>
2023-10-16 23:04:13 +0530

    added basic company dashboard

---
+++ FrontEnd/app/src/components/CompanyDashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function CompanyDashboard() {
+  // Company-specific dashboard content
+
+  return (
+    <div>
+      <h2>Company Dashboard</h2>
+      {/* Add components and logic for managing employees, updating salaries, and viewing transactions */}
+    </div>
+  );
+}
+
+export default CompanyDashboard;
