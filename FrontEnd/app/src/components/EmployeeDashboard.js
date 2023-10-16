d7ee32342cf9fb35e5e4ce91aac585be63915655
srihar5ha <sri3har5ha@gmail.com>
2023-10-16 23:06:00 +0530

    employee dashboard component added

---
+++ FrontEnd/app/src/components/EmployeeDashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function EmployeeDashboard() {
+  // Employee-specific dashboard content
+
+  return (
+    <div>
+      <h2>Employee Dashboard</h2>
+      {/* Display transaction history and employee-related information */}
+    </div>
+  );
+}
+
+export default EmployeeDashboard;
