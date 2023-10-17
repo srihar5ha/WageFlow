59358dc5182d98165b7305d81a65496a07fb2b4a
srihar5ha <sri3har5ha@gmail.com>
2023-10-18 03:06:51 +0530

    employee dashboard props fixed

--- FrontEnd/app/src/components/EmployeeDashboard.js
+++ FrontEnd/app/src/components/EmployeeDashboard.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
-function EmployeeDashboard() {
+function EmployeeDashboard({eoa,userType,onLogout}) {
   // Employee-specific dashboard content
-
+  
   return (
     <div>
       <h2>Employee Dashboard</h2>
+      <p>EOA:{eoa}</p>
+      <p></p>
+      <p>userType:{userType}</p>
+
+     
       {/* Display transaction history and employee-related information */}
+      
     </div>
   );
 }
